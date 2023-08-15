8b9ad97edb532ec36169034cc44d09d069051821
Gadisa21 <133111999+Gadisa21@users.noreply.github.com>
2023-08-15 12:00:45 +0300

    Create print_fun.py

---
+++ print_fun.py
@@ -0,0 +1,7 @@
+if __name__ == '__main__':
+    n = int(input())
+    
+    # Iterate through the range from 1 to n+1
+    for i in range(1, n + 1):
+        # Print the number without a newline or space
+        print(i, end='')
