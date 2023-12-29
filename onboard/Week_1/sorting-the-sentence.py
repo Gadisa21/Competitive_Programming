45b2b823c8b213a3240c8672da159f125c73daaf
Gadisa21 <133111999+Gadisa21@users.noreply.github.com>
2023-12-29 17:16:56 +0300

    Added solution for Sorting the Sentence

---
+++ onboard/Week_1/sorting-the-sentence.py
@@ -0,0 +1,10 @@
+class Solution:
+    def sortSentence(self, s: str) -> str:
+        s=s.split(" ")
+        s.sort(key=lambda x:x[-1])
+        for i in range(len(s)):
+            s[i]=s[i][:len(s[i])-1]
+        
+        return ' '.join(s)
+    
+        
