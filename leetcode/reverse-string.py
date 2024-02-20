eaf2f24da9884f8c07855e8cd44145aced6af781
Gadisa21 <133111999+Gadisa21@users.noreply.github.com>
2024-02-20 18:57:51 +0300

    Add solution for Reverse String

---
+++ leetcode/reverse-string.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def reverseString(self, s):
+        """
+        :type s: List[str]
+        :rtype: None Do not return anything, modify s in-place instead.
+        """
+        l=0
+        r=len(s)-1
+        def swap(l,r):
+            if l>=r:
+                return
+            s[l],s[r]=s[r],s[l]
+            swap(l+1,r-1)
+        swap(l,r)
+
+        
+        
+        
+       
+
+
+        
