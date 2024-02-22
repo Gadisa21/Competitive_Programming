2f58a84926297e597928398231dfaefaa92862d0
Gadisa21 <133111999+Gadisa21@users.noreply.github.com>
2024-02-22 18:03:53 +0300

    Add solution for Merge Two Sorted Lists

--- leetcode/merge-two-sorted-lists.py
+++ leetcode/merge-two-sorted-lists.py
@@ -1,24 +1,31 @@
-
-class Solution(object):
-    def mergeTwoLists(self, list1, list2):
-        """
-        :type list1: Optional[ListNode]
-        :type list2: Optional[ListNode]
-        :rtype: Optional[ListNode]
-        """
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
         dummy=ListNode()
-        tail=dummy
-        while list1 and list2:
-            if list1.val>list2.val:
-                tail.next=list2
-                list2=list2.next
+        tem=dummy
+        def custom(node1,node2,temp):
+            if node1==None:
+                temp.next=node2
+                return
+            if node2==None:
+                temp.next=node1
+                return 
+            if node1.val<=node2.val:
+                temp.next=node1
+                custom(node1.next,node2,temp.next)
             else:
-                tail.next=list1
-                list1=list1.next
-            tail=tail.next
-        if list1:
-            tail.next=list1
-        elif list2:
-            tail.next=list2
+                temp.next=node2
+                custom(node1,node2.next,temp.next)
+            return temp
+        custom(list1,list2,tem)
         return dummy.next
+
+
+
+
+
         
