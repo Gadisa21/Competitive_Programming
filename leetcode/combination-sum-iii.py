29797e4f571d1b076038ea39b759d50e7d848090
Gadisa21 <133111999+Gadisa21@users.noreply.github.com>
2024-03-01 09:31:54 +0300

    Add solution for Combination Sum III

---
+++ leetcode/combination-sum-iii.py
@@ -0,0 +1,20 @@
+class Solution:
+    def combinationSum3(self, k: int, n: int) -> List[List[int]]:
+        nums=[1,2,3,4,5,6,7,8,9]
+        ans=[]
+        def bc(comb,total,i):
+            if len(comb)==k:
+                if total==n:
+                    ans.append(comb.copy())
+                return
+            if len(nums)<=i or total>n:
+                return 
+            comb.append(nums[i])
+            bc(comb,total+nums[i],i+1)
+            comb.pop()
+            bc(comb,total,i+1)
+        bc([],0,0)
+        return ans
+                    
+
+        
