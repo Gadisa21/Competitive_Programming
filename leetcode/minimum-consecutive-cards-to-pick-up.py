3a6609a3fcbb0b45d9434ba117511dd1129649eb
Gadisa21 <133111999+Gadisa21@users.noreply.github.com>
2024-03-02 12:13:56 +0300

    Add solution for Minimum Consecutive Cards to Pick Up

---
+++ leetcode/minimum-consecutive-cards-to-pick-up.py
@@ -0,0 +1,11 @@
+class Solution:
+    def minimumCardPickup(self, cards: List[int]) -> int:
+        dic={}
+        ans=float("inf")
+        
+        for i in range(len(cards)):
+            if cards[i] in dic:
+                ans=min(ans, i-dic[cards[i]]+1)
+            dic[cards[i]]=i
+        return -1 if ans==float("inf") else ans
+        
