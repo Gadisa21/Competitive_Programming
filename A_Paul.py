08c51daaa50a7e15b3f4868c6874e38d7ba2aa02
Gadisa21 <gadisadiriba25@gmail.com>
2024-03-19 11:23:01 +0300

    A_Paul

---
+++ A_Paul.py
@@ -0,0 +1,18 @@
+from collections import defaultdict
+t=int(input())
+
+for _ in range(t):
+    dic=defaultdict(int)
+
+    s=input()
+
+    for ch in s:
+        dic[ch]+=1
+    ans=0
+    for val in dic.values():
+
+        if val>=2:
+            ans+=2
+        else:
+            ans+=val
+    print(ans//2)
