823409b880be4e616590fa8d97ec899c3d621f6e
Gadisa21 <gadisadiriba25@gmail.com>
2024-03-19 11:23:55 +0300

    A_tasty

---
+++ A_tasty.py
@@ -0,0 +1,16 @@
+
+n, m = map(int, input().split())
+
+base_candies = n // m
+
+remainder = n % m
+
+candies = [base_candies] * m
+
+for i in range(remainder):
+    candies[i] += 1
+
+candies.reverse()
+
+
+print(" ".join(map(str, candies)))
