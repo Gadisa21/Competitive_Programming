3a78e17e371e9da3e27ecf3cac90302b47cb2514
Gadisa21 <gadisadiriba25@gmail.com>
2024-03-26 11:38:07 +0300

    D_Converter_Machine

---
+++ D_Converter_Machine.py
@@ -0,0 +1,10 @@
+# Read the number of test cases
+t = int(input().strip())
+
+# Process each test case
+for _ in range(t):
+    m = int(input().strip())
+    if m == 7:
+        print("Yes")
+    else:
+        print("No")
