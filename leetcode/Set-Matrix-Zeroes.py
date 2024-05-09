2335d0471756fdfb17f6e4c847c098e3ca50802f
Gadisa21 <gadisadiriba25@gmail.com>
2024-05-09 19:14:06 +0300

    leetcode problems

---
+++ leetcode/Set-Matrix-Zeroes.py
@@ -0,0 +1,22 @@
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        cols=[False]*len(matrix[0])
+        rows=[False]*len(matrix)
+
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                if matrix[i][j]==0:
+                    cols[j]=True
+                    rows[i]=True
+        for i in range(len(cols)):
+            if cols[i]:
+                for j in range(len(matrix)):
+                    matrix[j][i]=0
+        for i in range(len(rows)):
+            if rows[i]:
+                for j in range(len(matrix[0])):
+                    matrix[i][j]=0
+        
